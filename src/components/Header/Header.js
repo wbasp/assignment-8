fb59ee9d3be3a054452a245eed1142a89733e3fa
wbasp <maad7445@gmail.com>
2023-03-05 13:52:25 +0600

    Header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Animal Home</h1>
+            <h3>Choose 4 Animals</h3>
+        </div>
+    );
+};
+
+export default Header;
